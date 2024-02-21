050566b38969d5dd546a0e32ad5de974179f11b7
Zack0079 <113079573+Zack0079@users.noreply.github.com>
2024-02-21 14:09:07 -0500

    Create the Ui dropdown List to update the strategy

--- Assets/Scripts/Working/ArrivalProcess.cs
+++ Assets/Scripts/Working/ArrivalProcess.cs
@@ -10,16 +10,16 @@ public class ArrivalProcess : MonoBehaviour
     public Transform personSpawnPlace;
 
     public float arrivalRateAsPersonsPerHour = 396.2f; // person/hour
-    public float interArrivalTimeInHours; 
+    public float interArrivalTimeInHours;
     private float interArrivalTimeInMinutes;
     private float interArrivalTimeInSeconds;
 
     public bool generateArrivals = true;
 
-    public float minInterArrivalTimeInSeconds = 3; 
+    public float minInterArrivalTimeInSeconds = 3;
     public float maxInterArrivalTimeInSeconds = 60;
 
-    public float a=3, b=7, c=5;
+    public float a = 3, b = 7, c = 5;
     //
     public enum ArrivalIntervalTimeStrategy
     {
@@ -30,7 +30,7 @@ public class ArrivalProcess : MonoBehaviour
         TriangularDistribution
     }
 
-    public ArrivalIntervalTimeStrategy arrivalIntervalTimeStrategy=ArrivalIntervalTimeStrategy.UniformIntervalTime;
+    public ArrivalIntervalTimeStrategy arrivalIntervalTimeStrategy = ArrivalIntervalTimeStrategy.UniformIntervalTime;
 
     QueueManager queueManager;
 
@@ -44,44 +44,50 @@ public class ArrivalProcess : MonoBehaviour
         interArrivalTimeInSeconds = interArrivalTimeInMinutes * 60;
         StartCoroutine(GenerateArrivals());
     }
-   
+
     IEnumerator GenerateArrivals()
     {
         while (generateArrivals)
         {
-            GameObject personGO=Instantiate(personPrefab, personSpawnPlace.position, Quaternion.identity);
+
+            GameObject personGO = Instantiate(personPrefab, personSpawnPlace.position, Quaternion.identity);
 
             float timeToNextArrivalInSec = interArrivalTimeInSeconds;
             float U = Random.value;
             switch (arrivalIntervalTimeStrategy)
             {
                 case ArrivalIntervalTimeStrategy.ConstantIntervalTime:
-                    timeToNextArrivalInSec= interArrivalTimeInSeconds;
+                    timeToNextArrivalInSec = interArrivalTimeInSeconds;
                     break;
                 case ArrivalIntervalTimeStrategy.UniformIntervalTime:
                     timeToNextArrivalInSec = Random.Range(minInterArrivalTimeInSeconds, maxInterArrivalTimeInSeconds);
                     break;
                 case ArrivalIntervalTimeStrategy.ExponentialIntervalTime:
-                    
+
                     float Lambda = 1 / arrivalRateAsPersonsPerHour;
-                    timeToNextArrivalInSec = Utilities.GetExp(U,Lambda);
+                    timeToNextArrivalInSec = Utilities.GetExp(U, Lambda);
                     break;
                 case ArrivalIntervalTimeStrategy.ObservedIntervalTime:
                     timeToNextArrivalInSec = interArrivalTimeInSeconds;
                     break;
                 case ArrivalIntervalTimeStrategy.TriangularDistribution:
-                    timeToNextArrivalInSec = Utilities.GetTriangularDistribution(U, a,b,c);
+                    timeToNextArrivalInSec = Utilities.GetTriangularDistribution(U, a, b, c);
                     break;
                 default:
                     print("No acceptable arrivalIntervalTimeStrategy:" + arrivalIntervalTimeStrategy);
                     break;
 
             }
+                    print("timeToNextArrivalInSec: " + timeToNextArrivalInSec);
 
-        yield return new WaitForSeconds(timeToNextArrivalInSec);
+            yield return new WaitForSeconds(timeToNextArrivalInSec);
 
 
         }
 
+    }
+    public void UpdateStrategy(int index)
+    {
+        arrivalIntervalTimeStrategy = (ArrivalIntervalTimeStrategy)index;
     }
 }
